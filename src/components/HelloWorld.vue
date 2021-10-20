c86c927c6d45f4322f412fc85a4ff11a434a903c
ibarretom <igorbm.23@gmail.com>
2021-10-20 15:02:26 -0300

    menu

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,7 +11,7 @@
             <v-list-item-group v-model="selectedItem" color="primary">
               <v-list-item v-for="(item, i) in items" :key="i">
                 <v-list-item-content>
-                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                  <v-list-item-title v-text="item.titulo"></v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
             </v-list-item-group>

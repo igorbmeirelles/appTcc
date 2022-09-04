33bb7bd8a39ad13eda5437a2287fc78466f14d4c
ibarretom <igorbm.23@gmail.com>
2022-09-04 19:08:49 -0300

    Adição de endereço da UFF

--- src/views/QuemSomos.vue
+++ src/views/QuemSomos.vue
@@ -19,7 +19,19 @@
         Your browser does not support the video tag.
       </video>
     </div>
-    <a href="https://forms.gle/wwXZevP5VRK73j9a8" target="_blank">
+
+    <h3>Voce sabia que a UFF tem um setor especializado em IST?</h3>
+    <address>
+      <p>Localizado em: Universidade Federal Fluminense - Campus Valonguinho</p>
+      <p>
+        Endereço: R. Prof. Hernani Pires de Mello, 71 - Ingá, Niterói - RJ,
+        24210-130
+      </p>
+      <p>Aberto 09:00h ⋅ Fecha às 22:00h</p>
+      <p>Telefone: (21) 2629-2494</p>
+    </address>
+    <h3>Diga o que achou do aplicativo</h3>
+    <a href="https://forms.gle/wwXZevP5VRK73j9a8" class="link" target="_blank">
       Deixe sua opinião clicando aqui!
     </a>
   </main>
@@ -29,5 +41,16 @@ main {
   max-width: 47.5rem;
   margin: auto;
   padding: 0 1rem;
+}
+address {
+  margin-bottom: 1rem;
+}
+
+p {
+  margin-bottom: 0.25rem
+}
+.link {
+  display: inline-block;
+  margin-bottom: 2rem;
 }
 </style>

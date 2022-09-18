177f9929d2dc327f1948c56b78655d55b597a733
ibarretom <igorbm.23@gmail.com>
2022-09-18 20:31:16 -0300

    Modificações na pagina de quem somos

--- src/views/QuemSomos.vue
+++ src/views/QuemSomos.vue
@@ -19,21 +19,48 @@
         Your browser does not support the video tag.
       </video>
     </div>
+    <h3>Equipe envolvida</h3>
+    <div>
+      <p>Igor Barreto Meirelles - Cederj - Ciências da Computação</p>
+      <p>João Victor Manço Resende - Acad. Enfermagem - Uff</p>
+      <p>
+        Jorge Luiz Lima - Prof. Saúde coletiva e políticas públicas - Mep/ Uff
+      </p>
+    </div>
 
-    <h3>Voce sabia que a UFF tem um setor especializado em IST?</h3>
+    <h3 class="mt-4">
+      Você sabia que a UFF tem um setor especializado em IST?
+    </h3>
+    <a href="http://www.dst.uff.br/" target="_blank">saiba mais clicando aqui</a>
     <address>
-      <p>Localizado em: Universidade Federal Fluminense - Campus Valonguinho</p>
+      <p>
+        Localizado em:
+        <a
+          href="https://www.google.com.br/maps/place/Universidade+Federal+Fluminense+-+Campus+Valonguinho/@-22.897877,-43.1279905,17z/data=!3m1!4b1!4m5!3m4!1s0x9983da8f8ec235:0x410efa02ccd4ae8!8m2!3d-22.897882!4d-43.1257965"
+          target="_blank"
+        >
+          Universidade Federal Fluminense - Campus Valonguinho
+        </a>
+      </p>
       <p>
         Endereço: R. Prof. Hernani Pires de Mello, 71 - Ingá, Niterói - RJ,
         24210-130
       </p>
-      <p>Aberto 09:00h ⋅ Fecha às 22:00h</p>
+      <p>Aberto 09h ⋅ Fecha às 22h</p>
       <p>Telefone: (21) 2629-2494</p>
     </address>
     <h3>Diga o que achou do aplicativo</h3>
     <a href="https://forms.gle/wwXZevP5VRK73j9a8" class="link" target="_blank">
       Deixe sua opinião clicando aqui!
     </a>
+
+    <footer class="mt-4 pb-4">
+      Este produto faz parte do Programa de Ensino Pesquisa e Extensão Espaço
+      Aberto para Saúde:
+      <a href="https://www.instagram.com/espacoabertosaudeuff/" target="_blank">
+        saiba mais
+      </a>
+    </footer>
   </main>
 </template>
 <style scoped>
@@ -47,10 +74,10 @@ address {
 }
 
 p {
-  margin-bottom: 0.25rem
+  margin-bottom: 0.25rem;
 }
 .link {
   display: inline-block;
-  margin-bottom: 2rem;
+  margin-bottom: 1rem;
 }
 </style>

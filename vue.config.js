3c5f79ba9659abf1e640c50974c641fdb0fa3363
ibarretom <igorbm.23@gmail.com>
2023-07-24 21:52:56 -0300

    mudanças no manifest.json

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,27 @@
 module.exports = {
   lintOnSave: false,
-
   transpileDependencies: ["vuetify"],
   pwa: {
-    name: 'Prev-IST',
-    short_name: 'Prev-IST',
-    themeColor: '#00bcd4',
-    msTileColor: '#00bcd4',
-    background_color: '#00bcd4',
-    appleMobileWebAppStatusBarStyle: '#00bcd4',
+    name: "Prev-IST",
+    start_url: ".",
+    short_name: "Prev-IST",
+    themeColor: "#00bcd4",
+    msTileColor: "#00bcd4",
+    background_color: "#00bcd4",
+    appleMobileWebAppStatusBarStyle: "#00bcd4",
     manifestOptions: {
-      background_color: '#00bcd4'
-    }
-  }
+      background_color: "#00bcd4",
+    },
+    display: "standalone",
+    lang: "pt",
+    orientation: "portrait",
+    display_override: [
+      "browser",
+      "fullscreen",
+      "minimal-ui",
+      "standalone",
+      "window-controls-overlay",
+    ],
+    categories: ["education", "health", "medical"],
+  },
 };

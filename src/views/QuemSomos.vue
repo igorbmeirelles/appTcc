3efdb623e084d23ab4e06ee0b4cb2ca386829546
igorbmeirelles <igorbm.23@gmail.com>
2026-04-29 16:17:21 -0300

    fix: video quem somos

--- src/views/QuemSomos.vue
+++ src/views/QuemSomos.vue
@@ -11,12 +11,7 @@
       class="mb-4"
       style="display: flex; justify-content: center; margin-top: 32px"
     >
-      <iframe 
-          width="350"
-          height="200" 
-          src="https://www.youtube.com/embed/b-9wAtI0O4E?si=ryzNpXxA9RKwAW7u" 
-          title="YouTube video player" 
-          frameborder="0" ></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/JFbJfLnbGT4?si=rMCz6JICHWktMHDH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
     </div>
     <h3>Equipe envolvida</h3>
